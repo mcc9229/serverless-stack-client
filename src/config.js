95e54bd4be3bf5a16093eadcc406ae67bfe6a2dc
Manuel Carrillo Castañeda <manuel_carrillo_90@hotmail.com>
2019-11-27 23:51:56 -0600

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-west-2",
+    BUCKET: "notes-app-uploads-90"
+  },
+  apiGateway: {
+    REGION: "us-west-2",
+    URL: "https://hh705s4vqh.execute-api.us-west-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-west-2",
+    USER_POOL_ID: "us-west-2_NqsH3wr7Q",
+    APP_CLIENT_ID: "7o8iv9s6hf4l1ne72bss0vmh93",
+    IDENTITY_POOL_ID: "us-west-2:7b335a80-10e6-47a8-beb6-dd1aab2ec209"
+  }
+};
